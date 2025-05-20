000229c8d75bb9f5f98e96f982a2b69f9b218175
Darren <xueyublue@gmail.com>
2025-05-20 23:02:02 +0800

    32:00'

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import "./chat.css";
 
 const Chat = () => {
-  return <div className="chat">Chat</div>;
+  return (
+    <div className="chat">
+      <div className="top">top</div>
+      <div className="center">center</div>
+      <div className="bottom">bottom</div>
+    </div>
+  );
 };
 
 export default Chat;
--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import "./chatList.css";
 
 const ChatList = () => {
+  const [addMode, setAddMode] = useState(false);
   return (
     <div className="chatList">
       <div className="search">
@@ -9,7 +10,33 @@ const ChatList = () => {
           <img src="./search.png" alt="" />
           <input type="text" placeholder="Search" />
         </div>
-        <img src="./plus.png" alt="" />
+        <img
+          src={addMode ? "./minus.png" : "./plus.png"}
+          alt=""
+          className="add"
+          onClick={() => setAddMode((prev) => !prev)}
+        />
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello, are you there?</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Go...</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>This is something that you should not know.</p>
+        </div>
       </div>
     </div>
   );
