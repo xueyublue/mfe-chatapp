eeb68f49b1f6cf995fd545f420264cbe20f245c7
Darren <xueyublue@gmail.com>
2025-05-20 23:21:56 +0800

    35:31'

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -4,7 +4,20 @@ import "./chat.css";
 const Chat = () => {
   return (
     <div className="chat">
-      <div className="top">top</div>
+      <div className="top">
+        <div className="user">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>Jane Doe</span>
+            <p>Lorem ipsum dolor adipisicing elit. Nam, aut?</p>
+          </div>
+        </div>
+        <div className="icons">
+          <img src="./phone.png" alt="" />
+          <img src="./video.png" alt="" />
+          <img src="./info.png" alt="" />
+        </div>
+      </div>
       <div className="center">center</div>
       <div className="bottom">bottom</div>
     </div>
