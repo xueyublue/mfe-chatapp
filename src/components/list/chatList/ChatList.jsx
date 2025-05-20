d20b026fd0a63da5a68e2ed80a36a8b8f3a10340
Darren <xueyublue@gmail.com>
2025-05-20 22:37:18 +0800

    25:31

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import "./chatList.css";
 
 const ChatList = () => {
-  return <div className="chatList">Chat List</div>;
+  return (
+    <div className="chatList">
+      <div className="search">
+        <div className="searchBar">
+          <img src="./search.png" alt="" />
+          <input type="text" placeholder="Search" />
+        </div>
+        <img src="./plus.png" alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default ChatList;
